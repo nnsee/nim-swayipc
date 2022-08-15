3b7a11cf47fc888ed53e44af7d5c3acf6edc3f8f
Rasmus Moorats <xx@nns.ee>
2022-08-15 10:59:01 +0300

    use better syntax for template calls

--- src/swayipc/util.nim
+++ src/swayipc/util.nim
@@ -15,14 +15,14 @@ proc filterNodesByClass*(parent: t_node, class: string, n = 0): seq[t_node] =
   var i: int
   if parent.window_properties.isSome:
     if parent.window_properties.get.class == class:
-      add_node_and_check(parent)
-  loop_and_add(filterNodesByClass, parent.nodes, class)
-  loop_and_add(filterNodesByClass, parent.floating_nodes, class)
+      parent.add_node_and_check
+  filterNodesByClass.loop_and_add(parent.nodes, class)
+  filterNodesByClass.loop_and_add(parent.floating_nodes, class)
 
 proc filterNodesByAppID*(parent: t_node, app_id: string, n = 0): seq[t_node] =
   var i: int
   if parent.app_id.isSome:
     if parent.app_id.get == app_id:
-      add_node_and_check(parent)
-  loop_and_add(filterNodesByAppID, parent.nodes, app_id)
-  loop_and_add(filterNodesByAppID, parent.floating_nodes, app_id)
+      parent.add_node_and_check
+  filterNodesByAppID.loop_and_add(parent.nodes, app_id)
+  filterNodesByAppID.loop_and_add(parent.floating_nodes, app_id)
